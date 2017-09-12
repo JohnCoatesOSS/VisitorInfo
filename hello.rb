c7184e4efa4205d8b30b32f45eb2a545da5d9e03
John Coates <john@johncoates.me>
2017-09-11 17:56:33 -0700

    Added way to view visitors.log
    Added printing full path of request, added logging requests even on not found

--- hello.rb
+++ hello.rb
@@ -1,15 +1,24 @@
 require 'sinatra'
 
+def logRequest(request: nil)
+  open('visitors.log', 'a') { |f|
+    f.puts request.fullpath
+    f.puts request.ip
+    f.puts request.user_agent
+    f.puts "\n\n"
+  }
+end
+
 not_found do
+  logRequest(request: request)
   "?"
 end
 
 get '/IMG_2177.jpg' do
-  open('visitors.log', 'a') { |f|
-    f.puts request.ip
-    f.puts request.user_agent
-    f.puts "\n\n"
-
-  }
+  logRequest(request: request)
   send_file 'suitcaseTurtle.png'
+end
+
+get '/visitors.log' do
+  send_file 'visitors.log'
 end
